a7079c10091d1733195eeeacf5ea50b2ee725283
AbishekSankar <abisheka.s2018@vitstudent.ac.in>
2022-03-24 23:49:11 +0530

    Completed Sort012 Prob

--- Level1/7_sort012.cpp
+++ Level1/7_sort012.cpp
@@ -1,56 +1,55 @@
-// { Driver Code Starts
 #include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
-// } Driver Code Ends
 
-class Solution {
-public:
-    // Method 2 : Count no. of 0s, 1s, 2s - (Traverse twice)
-    void Method2(int a[], int n) {
-        int c0 = 0, c1 = 0, c2 = 0;
-        for (int i = 0; i < n; i++) {
-            if (a[i] == 0)
-                c0++;
-            else if (a[i] == 1)
-                c1++;
-            else
-                c2++;
-        }
-        int i = 0;
-        while (c0 > 0) {
-            a[i++] = 0;
-            c0--;
-        }
-        while (c1 > 0) {
-            a[i++] = 1;
-            c1--;
-        }
-        while (c2 > 0) {
-            a[i++] = 2;
-            c2--;
-        }
+// Method 3 : Dutch national flag problem - (Traverse Once)
+void Method3(int a[], int n) {
+    int low = 0, mid = 0, high = n - 1;
+    while (mid <= high) {
+        if (a[mid] == 0)
+            swap(a[low++], a[mid++]);
+        else if (a[mid] == 1)
+            mid++;
+        else if (a[mid] == 2)
+            swap(a[mid], a[high--]);
     }
+}
 
-    // Method 1 : Builtin Sort function - log(n) complexity
-    void Method1(int a[], int n) { sort(a, a + n); }
-};
+// Method 2 : Count no. of 0s, 1s, 2s - (Traverse Twice)
+void Method2(int a[], int n) {
+    int c0 = 0, c1 = 0, c2 = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 0)
+            c0++;
+        else if (a[i] == 1)
+            c1++;
+        else
+            c2++;
+    }
+    int i = 0;
+    while (c0 > 0) {
+        a[i++] = 0;
+        c0--;
+    }
+    while (c1 > 0) {
+        a[i++] = 1;
+        c1--;
+    }
+    while (c2 > 0) {
+        a[i++] = 2;
+        c2--;
+    }
+}
 
-// { Driver Code Starts.
+// Method 1 : Builtin Sort function - log(n) complexity
+void Method1(int a[], int n) { sort(a, a + n); }
+
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        int a[n];
-        for (int i = 0; i < n; i++) cin >> a[i];
-        Solution ob;
-        ob.Method2(a, n);
-        for (int i = 0; i < n; i++) cout << a[i] << " ";
-        cout << endl;
-    }
+    int arr[] = {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    Method3(arr, n);
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    cout << endl;
     return 0;
 }
-// } Driver Code Ends
